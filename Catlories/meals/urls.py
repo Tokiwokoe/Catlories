07fd8af5724378774d2b403b4a717790f79edbf8
Tokiwokoe <mam4ur.anastasia@gmail.com>
2023-08-01 20:07:52 +0300

    ingredient information

--- Catlories/meals/urls.py
+++ Catlories/meals/urls.py
@@ -3,7 +3,7 @@ from meals import views
 from meals.views import GetMealsByDate, IngredientSearchView
 
 urlpatterns = [
-    path('meals/', views.diary, name='diary'),
+    path('', views.diary, name='diary'),
     path('add_meal/', views.add_meal, name='add_meal'),
     path('get_meals/<str:date_str>/', GetMealsByDate.as_view(), name='get_meals_by_date'),
     path('ingredient_search/', IngredientSearchView.as_view(), name='ingredient_search'),
--- Catlories/meals/views.py
+++ Catlories/meals/views.py
@@ -1,9 +1,11 @@
+from django.core.handlers import exception
 from django.shortcuts import render
+from django.views import View, defaults
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Meal, Ingredient
-from .serializers import MealSerializer, IngredientSerializer
+from .serializers import MealSerializer
 from datetime import datetime
 
 
@@ -18,18 +20,26 @@ class GetMealsByDate(APIView):
             return Response({'error': 'Неверная дата'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class IngredientSearchView(APIView):
+class IngredientSearchView(View):
     def get(self, request):
         code = request.GET.get('code', None)
+        meal_type = request.GET.get('meal_type', None)
+        date = request.GET.get('date', None)
         if code:
             try:
+                context = {}
                 ingredient = Ingredient.objects.get(code=code)
-                serializer = IngredientSerializer(ingredient)
-                return Response(serializer.data)
+                context['name'] = ingredient.name
+                context['kcal_per_100g'] = ingredient.kcal_per_100g
+                context['carbs_per_100g'] = ingredient.carbs_per_100g
+                context['protein_per_100g'] = ingredient.protein_per_100g
+                context['fat_per_100g'] = ingredient.fat_per_100g
+                print(meal_type, date)
+                return render(request, 'meals/ingredient_page.html', context)
             except Ingredient.DoesNotExist:
-                return Response({'detail': 'Продукт не найден'}, status=404)
+                return defaults.page_not_found(request, exception, template_name='404.html')
         else:
-            return Response({'detail': 'Введите штрих-код продукта'}, status=400)
+            return defaults.page_not_found(request, exception, template_name='404.html')
 
 
 def diary(request):
